ac6794e4b292a92c418cdcda45ada63cd4a4eb14
Taka <jamie.w@inwinstack.com>
2020-10-15 17:23:08 +0800

    patch cuda_arch threshold

--- faiss/gpu/utils/DeviceDefs.cuh
+++ faiss/gpu/utils/DeviceDefs.cuh
@@ -17,9 +17,8 @@ namespace faiss { namespace gpu {
 #error "CUDA >= 8.0 is required"
 #endif
 
-#define __CUDA_ARCH__ 800
 #ifdef __CUDA_ARCH__
-#if __CUDA_ARCH__ <= 800
+#if __CUDA_ARCH__ <= 1000
 constexpr int kWarpSize = 32;
 #else
 #error Unknown __CUDA_ARCH__; please define parameters for compute capability
